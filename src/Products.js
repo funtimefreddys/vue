2f5ade33a04dfd2a0cb2fc0405f6b365cd5642d0
GuskungTH <82573786+funtimefreddys@users.noreply.github.com>
2025-07-04 18:48:26 +0700

    Create Products.js

---
+++ src/Products.js
@@ -0,0 +1,72 @@
+export default [
+  {
+    id: 1,
+    name: "Oneplus 13R (Abroad)",
+    detail: "6.74\" AMOLED, Snapdragon 8+ Gen1, 50MP, 5500mAh, 100W",
+    coverimage: "Oneplus 13r.jpg",
+    price: "19,368 THB(Excluding Tax)"
+  },
+  {
+    id: 2,
+    name: "Oppo Find X8 Pro",
+    detail: "6.82\" QHD+ AMOLED, Dimensity 9400, 50MP Triple, 5000mAh, 100W",
+    coverimage: "Oppo  Find X8 Pro.jpg",
+    price: "39,900 THB"
+  },
+  {
+    id: 3,
+    name: "Oppo Reno12F",
+    detail: "6.67\" AMOLED, Dimensity 6300, 64MP, 5000mAh, 45W",
+    coverimage: "Oppo Reno12 F 5g.jpg",
+    price: "11,999 THB"
+  },
+  {
+    id: 4,
+    name: "Samsung Galaxy A56 5G",
+    detail: "6.6\" Super AMOLED, Exynos 1480, 50MP, 5000mAh, 25W",
+    coverimage: "samsung A56 5G.jpg",
+    price: "13,900 THB"
+  },
+  {
+    id: 5,
+    name: "Samsung Galaxy S25 plus",
+    detail: "6.7\" AMOLED QHD+, Snapdragon 8 Gen4, 50MP, ~4700mAh",
+    coverimage: "Samsung Galaxy S25 plus.jpg",
+    price: "30,900.00 THB"
+  },
+  {
+    id: 6,
+    name: "Samsung Galaxy S25 Ultra",
+    detail: "6.8\" AMOLED LTPO, Snapdragon 8 Gen4, 200MP, 5000mAh",
+    coverimage: "Samsung Galaxy S25 Ultra.jpg",
+    price: "45,900.00 THB"
+  },
+  {
+    id: 7,
+    name: "Samsung Galaxy A55 5G",
+    detail: "6.6\" Super AMOLED, Exynos 1480, 50MP, 5000mAh, 25W",
+    coverimage: "Samsung-a55.jpg",
+    price: "11,200 THB"
+  },
+  {
+    id: 8,
+    name: "Vivo X200 pro",
+    detail: "6.78\" LTPO AMOLED, Dimensity 9400, 50MP Zeiss, ~5100mAh",
+    coverimage: "Vivo X200 pro.jpg",
+    price: "38,200 THB"
+  },
+  {
+    id: 9,
+    name: "Xiaomi 14t pro",
+    detail: "6.67\" AMOLED, Dimensity 9300, 50MP Leica, 5000mAh, 120W",
+    coverimage: "Xiaomi 14t pro.jpg",
+    price: "20,100 THB"
+  },
+  {
+    id: 10,
+    name: "Xiaomi 15 Ultra",
+    detail: "6.73\" AMOLED LTPO, Snapdragon 8 Gen3, 50MP Quad, 5300mAh, 90W",
+    coverimage: "Xiaomi 15 Ultra.jpg",
+    price: "39,800 THB"
+  }
+];
