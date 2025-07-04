7d4472eab408a0796578d8aaab9e23d5b6b26fb9
GuskungTH <82573786+funtimefreddys@users.noreply.github.com>
2025-07-04 18:48:51 +0700

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <MyBar />
+  </header>
+  <main>
+    <router-view />
+  </main>
+</template>
+<script setup>
+import MyBar from './components/MyBar.vue'
+</script>
+<script>
+export default {
+  name : 'App',
+  components : {
+    MyBar
+  },
+};
+
+</script>
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-column-gap: 0.5rem;
+  grid-row-gap: 0.5rem;
+  justify-content: center;
+}
+
+@media (min-width: 576px) {
+  .grid-container {grid-template-columns: repeat(1, 1fr);}
+  }
+  @media (min-width: 992px) {
+  .grid-container {grid-template-columns: repeat(3, 1fr);}
+  }
+
+</style>
